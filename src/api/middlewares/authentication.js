5f0f2f5efd0b3c7e0417663aafa6d4957500f826
Yohanes-505 <535250054.yohanes@stu.untar.ac.id>
2026-04-18 23:24:06 +0700

    Endpoint bagian Yohanes

---
+++ src/api/middlewares/authentication.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+
+const { Users } = require('../../models');
+
+passport.use(
+  'user',
+  new passportJWT.Strategy(
+    {
+      jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    async (payload, done) => {
+      const user = await Users.findOne({ email: payload.email });
+
+      // user not found
+      if (!user) {
+        return done(null, false);
+      }
+
+      // user found
+      return done(null, user);
+    }
+  )
+);
+
+module.exports = passport.authenticate('user', { session: false });

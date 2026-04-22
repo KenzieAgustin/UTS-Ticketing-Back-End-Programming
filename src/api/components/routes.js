537a61f4a582005be7511c81d41398439903818e
Jessica-j3slyn <jj.jessicajeslyn@gmail.com>
2026-04-22 12:59:43 +0700

    halopak

--- src/api/components/routes.js
+++ src/api/components/routes.js
@@ -4,10 +4,14 @@ const auth = require('./auth/auth-route');
 const users = require('./users/users-route');
 const movie = require('./movies/movie-routes');
 const beverage = require('./beverage/beverage-routes');
+const theathers = require('./theathers/theathers-route'); // Cek pathnya!
+const showtimes = require('./showtimes/showtimes-route');
 
 module.exports = () => {
   const app = express.Router();
 
+  theathers(app);
+  showtimes(app);
   beverage(app);
   movie(app);
   auth(app);
